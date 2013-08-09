764072f786dd5b88801323cb852a429a651df94a
Han JU @ KXEN <ju.han.felix@gmail.com>
2013-08-09 17:23:06 +0200

    beginning crawler exercise

---
+++ src/main/scala/crawler/Manager.scala
@@ -0,0 +1,13 @@
+package crawler
+
+/**
+ *
+ */
+object Manager extends App {
+  val url = "http://www.cis.upenn.edu/~matuszek"
+  val pages = List(".htm", ".html")
+  val images = List(".gif", ".jpg", ".png")
+  val lectures = List(".ppt", ".pptx")
+
+
+}

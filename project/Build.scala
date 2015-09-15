04b61bb514ad83fd25468545b6d74b4bab871325
Han JU <ju.han.felix@gmail.com>
2015-09-15 15:16:59 +0200

    update scala to 2.11

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,12 @@ object ScalaExerciseBuild extends Build {
     // project settings
     version := "0.1.0-SNAPSHOT",
     organization := "me.juhanlol",
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.11.7",
     // dependencies
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "1.9.2" % "test",
-      "org.scala-lang" % "scala-swing" % "2.10.2",
-      "com.typesafe.akka" %% "akka-actor" % "2.2.1")
+      "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+      "org.scala-lang" % "scala-swing" % "2.11.0-M7",
+      "com.typesafe.akka" %% "akka-actor" % "2.3.13")
   )
 
   lazy val project = Project("scala-exercises", file("."),
